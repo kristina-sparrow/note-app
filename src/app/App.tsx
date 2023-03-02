dc936373e772f904f8e80b62419663cd2782d132
kristina-sparrow <118679841+kristina-sparrow@users.noreply.github.com>
2023-03-02 16:07:39 -0700

    Set up routes

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,5 +1,16 @@
 import React from "react";
+import { Routes, Route, Navigate } from "react-router-dom";
 
 export default function App() {
-  return <h1>Hello, World</h1>;
+  return (
+    <Routes>
+      <Route path="/" element={<h1>Home</h1>} />
+      <Route path="/new" element={<h1>New</h1>} />
+      <Route path="/:id">
+        <Route index element={<h1>Show</h1>} />
+        <Route path="edit" element={<h1>Edit</h1>} />
+      </Route>
+      <Route path="*" element={<Navigate to="/" />} />
+    </Routes>
+  );
 }
